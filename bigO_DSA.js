4dbc2ac08fffed18c73f3fee802c6353d0a7884f
Oluwatobiloba Sokoya <112967921+devtobi16@users.noreply.github.com>
2024-03-03 02:58:05 +0100

    Push=> LinkedList

--- bigO_DSA.js
+++ bigO_DSA.js
@@ -107,7 +107,22 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+  //Push Method 
+  push(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 let myLinkedList = new LinkedList(4);
+myLinkedList.push(5);
+console.log(myLinkedList);
 
