60c9fe27edcfbf9addefb2176d527e5557a4d44e
Oluwatobiloba Sokoya <112967921+devtobi16@users.noreply.github.com>
2024-04-09 02:52:01 +0100

    Update Best Time to Buy and Sell Stock.js

--- Best Time to Buy and Sell Stock.js
+++ Best Time to Buy and Sell Stock.js
@@ -20,3 +20,28 @@ function maxProfit(prices) {
     }
   }
 }
+
+//Kid with the greatest number of candies
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let max=0;
+    for(let i = 0;i<candies.length; i++){
+        if(candies[i]>max){
+            max = candies[i]
+        }
+    }
+  
+    for(let i =0;i<candies.length;i++){
+        if(( extraCandies+candies[i])>=max){
+            candies[i]= true;
+        }else{
+            candies[i] = false;
+        }
+    }
+    return candies;
+};
+//BigO => O(m+n);
